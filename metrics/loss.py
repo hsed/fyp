d069593f33804f8774fe92abe3611dc450f2599d
Haaris <hsed@users.noreply.github.com>
2019-04-20 20:53:12 +0100

    a sample test

--- metrics/loss.py
+++ metrics/loss.py
@@ -9,3 +9,17 @@ def nll_loss(output, target):
 
 def mse_loss(output, target):
     return F.mse_loss(output, target)
+
+
+
+
+def mse_and_nll_loss(outputs: tuple, targets: tuple):
+    '''
+        outputs and targets: (HandSkeleton3D, ActionClassIdx)  
+    '''
+
+    output_hpe, output_probvect = outputs
+    target_hpe, target_cidx = targets
+
+    # combine the two losses with eual weights, TODO: adjust loss weights in future...
+    return F.mse_loss(output_hpe, target_hpe) + F.nll_loss(output_probvect, target_cidx)
